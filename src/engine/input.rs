8356c22b177a1ddf662af35053efa52732bf5230
〽️ Gersi Muça <gm96key@gmail.com>
2026-06-25 21:41:56 -0400

    Create input.rs

---
+++ src/engine/input.rs
@@ -0,0 +1,27 @@
+use egui::InputState;
+use crate::engine::{Game};
+use crate::engine::selection::select_unit;
+use crate::engine::pathfinding::find_path;
+
+pub fn handle_input(game: &mut Game, input: &InputState) {
+    let pointer = input.pointer.hover_pos();
+    let clicked = input.pointer.primary_clicked();
+
+    if let Some(pos) = pointer {
+        let world_x = (pos.x / 32.0) as i32;
+        let world_y = (pos.y / 32.0) as i32;
+
+        if clicked {
+            if !select_unit(&mut game.units, world_x, world_y) {
+                let selected: Vec<_> = game.units
+                    .iter_mut()
+                    .filter(|u| u.selected)
+                    .collect();
+
+                for u in selected {
+                    u.set_path(find_path(&game.map, (u.x, u.y), (world_x, world_y)));
+                }
+            }
+        }
+    }
+}
